a6f0ff8eab4f72e5222e865c4ed4ecf37ca60095
ValKart <vlebental@gmail.com>
2022-03-22 22:14:47 +0300

    Задание №2 Калькулятор + Максимальный элемент массива

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -9,6 +9,11 @@ public class Array {
     this.in = in;
   }
   
+  /**
+   * метод выводит на консоль самое длинное слово в массиве,
+   * полученное вызовом метода findLongestWord(String[] array, int elementsQuantity)
+   */
+  
   public void start() {
     System.out.println("Введите количество элементов массива:");
     int elementsQuantity = in.nextInt();
@@ -16,30 +21,36 @@ public class Array {
     String[] stringArray = new String[elementsQuantity];
     
     System.out.println("Введите произвольные слова количеством " + elementsQuantity);
-    //StringBuilder str = new StringBuilder();
     
+    // заполнение массива введенными с консоли значениями
     for (int i = 0; i < elementsQuantity; i++) {
       stringArray[i] = in.next();
     }
-    System.out.println(Arrays.toString(stringArray));
+    //System.out.println(Arrays.toString(stringArray));
     
     String maxWord = findLongestWord(stringArray, elementsQuantity);
-    System.out.println("Самое длинное слово(а): " + maxWord);
+    System.out.println("Самое длинное слово: " + maxWord);
     
     in.close();
   }
   
+  
+  /**
+   *
+   * @param array массив строковых значений
+   * @param elementsQuantity количество элементов в массиве
+   * @return возвращает самое длинное слово в массиве
+   */
   private String findLongestWord(String[] array, int elementsQuantity) {
     int maxLength = array[0].length();
-    //String maxWord = array[0];
-    StringJoiner maxWord = new StringJoiner(", ");
+    String maxWord = array[0];
     
     for (int i = 0; i < elementsQuantity; i++) {
-      if (maxLength <= array[i].length()) {
-        maxWord.add(array[i]);
-        // а если таких слов несколько stringBuilder?
+      if (maxLength < array[i].length()) {
+        maxWord = array[i];
+        maxLength = array[i].length();
       }
     }
-    return maxWord.toString();
+    return maxWord;
   }
 }
