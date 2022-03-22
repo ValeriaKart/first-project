3d97eeb6d3e046cc562b54c3cc8acdad831e9cd7
ValKart <vlebental@gmail.com>
2022-03-22 13:43:27 +0300

    Задание №2 Калькулятор рефакторинг

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -1,27 +1,26 @@
 import java.util.Scanner;
 
 public class Base {
-
-    public static void main(String[] args) {
-        //Создаем объект класса Scanner для считывания значений с консоли и сохраняем в переменную in
-        Scanner in = new Scanner(System.in);
-        System.out.println("Выберите операцию. Введите 1 для выбора калькулятора, 2 для поиска элемента в массиве");
-        //Сохраняем в переменную operationType значение типа операции из консоли, возвращенное методом nextInt()
-        int operationType = in.nextInt();
-
-        if (operationType == 1) {
-            Calculator calculator = new Calculator(in);
-            calculator.start();
-        } else if (operationType == 2) {
-
-        } else {
-            System.out.println("Операция не поддерживается. Выберите 1 - калькулятор или 2 - поиск элемента в массиве");
-        }
-
-
-
-        in.close();
+  
+  public static void main(String[] args) {
+    //Создаем объект класса Scanner для считывания значений с консоли и сохраняем в переменную in
+    Scanner in = new Scanner(System.in);
+    System.out.println("Выберите операцию. Введите 1 для выбора калькулятора, 2 для поиска элемента в массиве");
+    //Сохраняем в переменную operationType значение типа операции из консоли, возвращенное методом nextInt()
+    int operationType = in.nextInt();
+    
+    if (operationType == 1) {
+      Calculator calculator = new Calculator(in);
+      calculator.start();
+    } else if (operationType == 2) {
+    
+    } else {
+      System.out.println("Операция не поддерживается. Выберите 1 - калькулятор или 2 - поиск элемента в массиве");
     }
-
-
+    
+    
+    in.close();
+  }
+  
+  
 }
--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,93 +1,81 @@
 import java.util.Scanner;
 
 public class Calculator {
-    private final Scanner in;
-
-    public Calculator(Scanner in) {
-        this.in = in;
-    }
-
-    public void start() {
-        System.out.println("Выберите операцию. Введите знак \"+\" для сложения, знак \"*\" для умножения, \"-\" - вычитания, \"\\\" - деления ");
-        //Сохраняем в переменную operation значение типа операции из консоли, возвращенное методом next()
-        String operation = in.next();
-
-        //Выводим пользователю просьбу ввести числа в консоль и сохраняем значения в переменные num1 и num2
-        System.out.println("Введите первое число в виде десятичной дроби с разделителем запятой:");
-        double num1 = in.nextDouble();
-        System.out.println("Введите второе число в виде десятичной дроби с разделителем запятой:");
-        double num2 = in.nextDouble();
+  private final Scanner in;
+  
+  public Calculator(Scanner in) {
+    this.in = in;
+  }
+  
+  public void start() {
+    System.out.println("Выберите операцию. Введите знак \"+\" для сложения, знак \"*\" для умножения, " +
+      "\"-\" - вычитания, \"\\\" - деления ");
+    // Сохраняем в переменную operation значение типа операции из консоли, возвращенное методом next()
+    String operation = in.next();
+    
+    // Выводим пользователю просьбу ввести числа в консоль и сохраняем значения в переменные num1 и num2
+    System.out.println("Введите первое число в виде десятичной дроби с разделителем запятой:");
+    double num1 = in.nextDouble();
+    System.out.println("Введите второе число в виде десятичной дроби с разделителем запятой:");
+    double num2 = in.nextDouble();
 
         /*Условие для выбора операции с введенными числами. Если "+" - сложение,
         "*" - умножение, "-" - вычитание, "\" - деление. Иначе сообщение, что операция не поддерживается*/
-        switch(operation) {
-            case "+":
-                System.out.printf("Результат = %.4f", add(num1, num2));
-                break;
-            case "*":
-                System.out.printf("Результат = %.4f", multiply(num1, num2));
-                break;
-            case "-":
-                System.out.printf("Результат = %.4f", subtract(num1, num2));
-                break;
-            case "/":
-                System.out.printf("Результат = %.4f", divide(num1, num2));
-                break;
-            default:
-                System.out.println("Введенная операция не поддерживается. Попробуйте снова, используя знаки +, *, -, /.");
-                break;
-        }
-
-        in.close();
+    switch (operation) {
+      case "+":
+        System.out.printf("Результат = %.4f", add(num1, num2));
+        break;
+      case "*":
+        System.out.printf("Результат = %.4f", multiply(num1, num2));
+        break;
+      case "-":
+        System.out.printf("Результат = %.4f", subtract(num1, num2));
+        break;
+      case "/":
+        System.out.printf("Результат = %.4f", divide(num1, num2));
+        break;
+      default:
+        System.out.println("Введенная операция не поддерживается. Попробуйте снова, используя знаки +, *, -, /.");
+        break;
     }
-
-    /**
-     *
-     * @param number1 - первое число в виде десятичной дроби с разделителем запятой
-     * @param number2 - второе число в виде десятичной дроби с разделителем запятой
-     * @return метод возвращает результат сложения двух чисел (number1 и number2)
-     */
-    private double add(double number1, double number2) {
-
-        return number1 + number2;
-    }
-
-    /**
-     *
-     * @param number1 - первое число в виде десятичной дроби с разделителем запятой
-     * @param number2 - второе число в виде десятичной дроби с разделителем запятой
-     * @return метод возвращает результат умножения двух чисел (number1 и number2)
-     */
-    private double multiply(double number1, double number2) {
-
-        return number1 * number2;
-
-    }
-
-    /**
-     *
-     * @param number1 - первое число в виде десятичной дроби с разделителем запятой
-     * @param number2 - второе число в виде десятичной дроби с разделителем запятой
-     * @return метод возвращает результат вычитания двух чисел (number1 и number2)
-     */
-    private double subtract(double number1, double number2) {
-
-        return number1 - number2;
-
-    }
-
-    /**
-     *
-     * @param number1 - первое число в виде десятичной дроби с разделителем запятой
-     * @param number2 - второе число в виде десятичной дроби с разделителем запятой
-     * @return метод возвращает результат деления двух чисел (number1 и number2), если второе не равно 0
-     */
-    private double divide(double number1, double number2) {
-
-        /*if (number2 == 0){
-            System.out.println("На ноль делить нельзя!");
-        }else{*/
-            return number1 / number2;
-        }
-    }
+    in.close();
+  }
+  
+  /**
+   * @param number1 - первое число в виде десятичной дроби с разделителем запятой
+   * @param number2 - второе число в виде десятичной дроби с разделителем запятой
+   * @return метод возвращает результат сложения двух чисел (number1 и number2)
+   */
+  private double add(double number1, double number2) {
+    return number1 + number2;
+  }
+  
+  /**
+   * @param number1 - первое число в виде десятичной дроби с разделителем запятой
+   * @param number2 - второе число в виде десятичной дроби с разделителем запятой
+   * @return метод возвращает результат умножения двух чисел (number1 и number2)
+   */
+  private double multiply(double number1, double number2) {
+    return number1 * number2;
+  }
+  
+  /**
+   * @param number1 - первое число в виде десятичной дроби с разделителем запятой
+   * @param number2 - второе число в виде десятичной дроби с разделителем запятой
+   * @return метод возвращает результат вычитания двух чисел (number1 и number2)
+   */
+  private double subtract(double number1, double number2) {
+    return number1 - number2;
+  }
+  
+  /**
+   * @param number1 - первое число в виде десятичной дроби с разделителем запятой
+   * @param number2 - второе число в виде десятичной дроби с разделителем запятой
+   * @return метод возвращает результат деления двух чисел (number1 и number2), если второе не равно 0
+   */
+  private double divide(double number1, double number2) {
+    // TODO: add exception on zero division
+    return number1 / number2;
+  }
+}
 
