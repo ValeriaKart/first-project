f448c10fc449a0c3c00bf12bb44ae6b5ce6cca7c
ValKart <vlebental@gmail.com>
2022-03-22 12:47:11 +0300

    Задание №2 Калькулятор добавление операций

--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -5,47 +5,14 @@ public class Base {
     public static void main(String[] args) {
         //Создаем объект класса Scanner для считывания значений с консоли и сохраняем в переменную in
         Scanner in = new Scanner(System.in);
-        System.out.println("Выберите операцию. Введите знак + для сложения, знак * для умножения");
-        //Сохраняем в переменную operationType значение типа операции из консоли, возвращенное методом next()
-        String operationType = in.next();
+        System.out.println("Выберите операцию. Введите 1 для выбора калькулятора, 2 для поиска элемента в массиве");
+        //Сохраняем в переменную operationType значение типа операции из консоли, возвращенное методом nextInt()
+        int operationType = in.nextInt();
 
-        //Выводим пользователю просьбу ввести числа в консоль и сохраняем значения в переменные num1 и num2
-        System.out.println("Введите первое число в виде десятичной дроби с разделителем запятой:");
-        double num1 = in.nextDouble();
-        System.out.println("Введите второе число в виде десятичной дроби с разделителем запятой:");
-        double num2 = in.nextDouble();
 
-        /*Условие для выбора операции с введенными числами. Если "+" - сложение,
-        "*" - умножение, иначе сообщение, что операция не поддерживается*/
-        if (operationType.equals("+")) {
-            System.out.printf("Результат = %.4f", additionCalculator(num1, num2));
-        } else if (operationType.equals("*")) {
-            System.out.printf("Результат = %.4f", multiplyCalculator(num1, num2));
-        } else {
-            System.out.println("Введенная операция не поддерживается. Попробуйте снова, используя знаки + или *.");
-        }
+
+        in.close();
     }
 
-    /**
-     *
-     * @param number1 - первое число в виде десятичной дроби с разделителем запятой
-     * @param number2 - второе число в виде десятичной дроби с разделителем запятой
-     * @return метод возвращает результат сложения двух чисел (number1 и number2)
-     */
-    public static double additionCalculator(double number1, double number2) {
 
-        return number1 + number2;
-    }
-
-    /**
-     *
-     * @param number1 - первое число в виде десятичной дроби с разделителем запятой
-     * @param number2 - второе число в виде десятичной дроби с разделителем запятой
-     * @return метод возвращает результат умножения двух чисел (number1 и number2)
-     */
-    public static double multiplyCalculator(double number1, double number2) {
-
-        return number1 * number2;
-
-    }
 }
---
+++ src/main/java/Calculator.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+    //Создаем объект класса Scanner для считывания значений с консоли и сохраняем в переменную in
+    Scanner in = new Scanner(System.in);
+        System.out.println("Выберите операцию. Введите знак \"+\" для сложения, знак \"*\" для умножения, \"-\" - вычитания, \"\\\" - деления ");
+    //Сохраняем в переменную operation значение типа операции из консоли, возвращенное методом next()
+    String operation = in.next();
+
+    //Выводим пользователю просьбу ввести числа в консоль и сохраняем значения в переменные num1 и num2
+        System.out.println("Введите первое число в виде десятичной дроби с разделителем запятой:");
+    double num1 = in.nextDouble();
+        System.out.println("Введите второе число в виде десятичной дроби с разделителем запятой:");
+    double num2 = in.nextDouble();
+
+        /*Условие для выбора операции с введенными числами. Если "+" - сложение,
+        "*" - умножение, "-" - вычитание, "\" - деление. Иначе сообщение, что операция не поддерживается*/
+    switch (operation){
+        case "+":  System.out.printf("Результат = %.4f", addition(num1, num2));
+            break;
+        case "*": System.out.printf("Результат = %.4f", multiply(num1, num2));
+            break;
+        default: System.out.println("Введенная операция не поддерживается. Попробуйте снова, используя знаки +, *, -, /.");
+        break;
+    }
+
+
+
+        if (operationType.equals("+")) {
+        System.out.printf("Результат = %.4f", addition(num1, num2));
+    } else if (operationType.equals("*")) {
+        System.out.printf("Результат = %.4f", multiply(num1, num2));
+    } else {
+        System.out.println("Введенная операция не поддерживается. Попробуйте снова, используя знаки + или *.");
+    }
+        in.close();
+
+    /**
+     *
+     * @param number1 - первое число в виде десятичной дроби с разделителем запятой
+     * @param number2 - второе число в виде десятичной дроби с разделителем запятой
+     * @return метод возвращает результат сложения двух чисел (number1 и number2)
+     */
+    public double addition(double number1, double number2) {
+
+        return number1 + number2;
+    }
+
+    /**
+     *
+     * @param number1 - первое число в виде десятичной дроби с разделителем запятой
+     * @param number2 - второе число в виде десятичной дроби с разделителем запятой
+     * @return метод возвращает результат умножения двух чисел (number1 и number2)
+     */
+    public double multiply(double number1, double number2) {
+
+        return number1 * number2;
+
+    }
+}
