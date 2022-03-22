54d88ccd8e1eb17e03ebe585bb4d528a34e1c8d5
ValKart <vlebental@gmail.com>
2022-03-22 19:25:01 +0300

    Задание №2 Максимальный элемент массива

--- src/main/java/Array.java
+++ src/main/java/Array.java
@@ -1,2 +1,45 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringJoiner;
+
 public class Array {
+  private final Scanner in;
+  
+  public Array(Scanner in) {
+    this.in = in;
+  }
+  
+  public void start() {
+    System.out.println("Введите количество элементов массива:");
+    int elementsQuantity = in.nextInt();
+    
+    String[] stringArray = new String[elementsQuantity];
+    
+    System.out.println("Введите произвольные слова количеством " + elementsQuantity);
+    //StringBuilder str = new StringBuilder();
+    
+    for (int i = 0; i < elementsQuantity; i++) {
+      stringArray[i] = in.next();
+    }
+    System.out.println(Arrays.toString(stringArray));
+    
+    String maxWord = findLongestWord(stringArray, elementsQuantity);
+    System.out.println("Самое длинное слово(а): " + maxWord);
+    
+    in.close();
+  }
+  
+  private String findLongestWord(String[] array, int elementsQuantity) {
+    int maxLength = array[0].length();
+    //String maxWord = array[0];
+    StringJoiner maxWord = new StringJoiner(", ");
+    
+    for (int i = 0; i < elementsQuantity; i++) {
+      if (maxLength <= array[i].length()) {
+        maxWord.add(array[i]);
+        // а если таких слов несколько stringBuilder?
+      }
+    }
+    return maxWord.toString();
+  }
 }
--- src/main/java/Base.java
+++ src/main/java/Base.java
@@ -3,24 +3,23 @@ import java.util.Scanner;
 public class Base {
   
   public static void main(String[] args) {
-    //Создаем объект класса Scanner для считывания значений с консоли и сохраняем в переменную in
+    // Создаем объект класса Scanner для считывания значений с консоли и сохраняем в переменную in
     Scanner in = new Scanner(System.in);
-    System.out.println("Выберите операцию. Введите 1 для выбора калькулятора, 2 для поиска элемента в массиве");
-    //Сохраняем в переменную operationType значение типа операции из консоли, возвращенное методом nextInt()
+    System.out.println("Выберите операцию. Введите 1 для выбора калькулятора, 2 для поиска максимального слова в массиве");
+    // Сохраняем в переменную operationType значение типа операции из консоли, возвращенное методом nextInt()
     int operationType = in.nextInt();
     
+    // Условие для выбора действия. 1 - калькулятор, 2 - поиск максимального слова в массиве
     if (operationType == 1) {
       Calculator calculator = new Calculator(in);
       calculator.start();
     } else if (operationType == 2) {
-    
+      Array array = new Array(in);
+      array.start();
     } else {
-      System.out.println("Операция не поддерживается. Выберите 1 - калькулятор или 2 - поиск элемента в массиве");
+      System.out.println("Операция не поддерживается. Выберите 1 - калькулятор или 2 - поиск максимального слова в массиве");
+      in.close();
     }
     
-    
-    in.close();
   }
-  
-  
 }
